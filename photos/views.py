53068e32698590251471c3d7baa975469db1641c
Arnoldblair <arnoldblair.munene@student.moringaschool.com>
2022-02-28 10:45:09 +0300

    add first view

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Image, Location, Category
+from django.http  import HttpResponse, Http404
 
 # Create your views here.
+def index(request):
+    images = Image.get_all_images()
+    locations = Location.objects.all()
+    title = 'My-art'
+
+    return render(request, 'index.html', {'title':title, 'images':images, 'locations':locations})
